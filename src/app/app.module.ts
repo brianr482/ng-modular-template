6fee17859a180965a7fa490f4e121c687f22e232
Brian Ramirez <brians@uninorte.edu.co>
2019-04-11 21:34:00 -0500

    chore: app module was updated    - imports arr updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/* Core & Shared module */
+import { SharedModule } from './shared';
+import { CoreModule } from './core';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    // angular
     BrowserModule,
-    AppRoutingModule
+
+    // core & shared
+    CoreModule,
+    SharedModule,
+
+    //  no lazy feature modules
+
+    // app
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
