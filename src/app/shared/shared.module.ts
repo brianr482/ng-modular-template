ec3696d23a4d5960a70063ec5f4e54e694a6f621
Brian Ramirez <brians@uninorte.edu.co>
2019-04-11 21:51:11 -0500

    chore: installing angular flex layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    FlexLayoutModule
   ],
   declarations: [],
   exports: [
     CommonModule,
     FormsModule,
+    FlexLayoutModule
   ]
 })
 export class SharedModule { }
