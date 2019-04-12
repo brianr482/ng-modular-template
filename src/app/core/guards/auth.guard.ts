b9d09c5926941cc9498d7a5be0f6b438f57d0b2d
Brian Ramirez <brians@uninorte.edu.co>
2019-04-11 23:24:38 -0500

    feat: update core with dummy data

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements  {
+  
+}
