86f59e88d70c9013a9207a824c075cd155cc040b
Brian Ramirez <brians@uninorte.edu.co>
2019-04-11 21:30:08 -0500

    feat: creating dummy feat

---
+++ src/app/feat/dummy-feat/dummy-feat.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '@app/shared';
+import { DummyFeatRoutingModule } from './dummy-feat-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    SharedModule,
+    DummyFeatRoutingModule
+  ]
+})
+export class DummyFeatModule { }
